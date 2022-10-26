d40be4a785fe8dca350b662b0e47d974d3524ac5
Michal Dobrzycki <dobrzycki@gmail.com>
2022-10-26 12:43:08 +0200

    Zadanie 1

--- prezentacja1/test_pb.py
+++ prezentacja1/test_pb.py
@@ -1,4 +1,15 @@
+class PhoneBook():
+    def __init__(self) -> None:
+        self.numbers = {}
+    
+    def add(self, name, number):
+        self.numbers[name] = number
+
+    def lookup(self, name):
+        return self.numbers[name]
+
+
 def test_lookup_by_name():
-    phonebook = Phonebook()
+    phonebook = PhoneBook()
     phonebook.add("Rossmann", "123456789")
-    assert "123456789" == phonebook.lookup("Bob")
+    assert "123456789" == phonebook.lookup("Rossmann")
